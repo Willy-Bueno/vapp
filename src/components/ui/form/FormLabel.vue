56825e6867e3af53ffbcd66985db77f1c76c8353
Willy-Bueno <willybueno090@gmail.com>
2023-10-26 15:42:35 -0300

    feat: adiciona o componente de formulário

---
+++ src/components/ui/form/FormLabel.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { Label } from 'radix-vue'
+import { useFormField } from './useFormField'
+import { cn } from '@/lib/utils'
+
+const { error, formItemId } = useFormField()
+</script>
+
+<template>
+  <Label
+    :class="cn(
+      'block text-sm tracking-tight font-medium text-foreground text-left',
+      error && 'text-destructive',
+      $attrs.class ?? '',
+    )"
+    :for="formItemId"
+  >
+    <slot />
+  </Label>
+</template>
